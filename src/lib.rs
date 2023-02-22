17139f6ae2557f764abffea8e95352a9bfcef900
spazzylemons <25674682+spazzylemons@users.noreply.github.com>
2023-02-22 12:19:07 -0500

    add license

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+//! Condux - an antigravity racing game
+//! Copyright (C) 2023 spazzylemons
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
+//!
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//! GNU General Public License for more details.
+//!
+//! You should have received a copy of the GNU General Public License
+//! along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 #![cfg_attr(target_os = "horizon", feature(allocator_api))]
 
 use ouroboros::self_referencing;
